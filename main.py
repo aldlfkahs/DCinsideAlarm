1f2182ec558fd81cd93c59dbfb5088681ebb1c11
Seungwon Lee <41567934+aldlfkahs@users.noreply.github.com>
2021-01-26 23:15:19 +0900

    [feat] 키워드 기능 추가 ver.1.3.0

--- main.py
+++ main.py
@@ -8,6 +8,9 @@
       https://www.element14.com/community/thread/57051/l/how-to-break-out-of-a-while-true-loop-with-a-button?displayFullThread=true
       https://stackoverflow.com/questions/37815371/pyinstaller-failed-to-execute-script-pyi-rth-pkgres-and-missing-packages
       https://stackoverflow.com/questions/16511337/correct-way-to-try-except-using-python-requests-module
+      https://wikidocs.net/21938
+      https://wikidocs.net/35496
+      https://wikidocs.net/36766
 '''
 '''
 사용 전 필수 설정사항
@@ -29,6 +32,7 @@ user_agent = {'User-agent': 'Mozilla/5.0'}
 flag = True
 recent = 1
 
+# url로 get 요청을 보내는 함수
 def get_html(url):
    _html = ""
    suc = False
@@ -60,22 +64,49 @@ class MyApp(QWidget):
       self.addr = QLineEdit("", self)
       self.addr.setText("https://gall.dcinside.com/mgallery/board/lists?id=aoegame")
 
+      text1 = QLabel('갤러리 주소')
+      togomi = QLabel('버전 : 1.3.0')
 
       btn1 = QPushButton('시작', self)
-      btn1.resize(50, 50)
       btn1.clicked.connect(self.button1Function)
-
       btn2 = QPushButton('중지', self)
-      btn2.resize(50, 50)
       btn2.clicked.connect(self.button2Function)
 
-      grid.addWidget(self.addr, 0, 0, 1, 2)
-      grid.addWidget(btn1, 1, 0)
-      grid.addWidget(btn2, 1, 1)
+      text2 = QLabel('키워드')
+
+      global keyword
+      keyword = QListWidget()
+
+      global k_on, k_off
+      k_on = QRadioButton('ON', self)
+      k_off = QRadioButton('OFF', self)
+      k_off.setChecked(True)
+
+      global newItem
+      newItem = QLineEdit()
+
+      btn3 = QPushButton('추가', self)
+      btn3.clicked.connect(self.button3Function)
+      btn4 = QPushButton('삭제', self)
+      btn4.clicked.connect(self.button4Function)
+
+
+      grid.addWidget(text1, 0, 0)
+      grid.addWidget(togomi, 0, 4)
+      grid.addWidget(self.addr, 1, 0, 1, 5)
+      grid.addWidget(btn1, 2, 0, 1, 4)
+      grid.addWidget(btn2, 2, 4)
+      grid.addWidget(text2, 3, 0)
+      grid.addWidget(k_on, 3, 1)
+      grid.addWidget(k_off, 3, 2)
+      grid.addWidget(newItem, 3, 3)
+      grid.addWidget(keyword, 4, 0, -1, 4)
+      grid.addWidget(btn3, 4, 4)
+      grid.addWidget(btn4, 5, 4)
 
       self.setWindowTitle('DC 새글 알리미')
       self.setWindowIcon(QIcon('icon.png'))
-      self.setFixedSize(380, 100)
+      self.setFixedSize(380, 200)
       self.center()
       self.show()
 
@@ -85,20 +116,20 @@ class MyApp(QWidget):
       qr.moveCenter(cp)
       self.move(qr.topLeft())
 
-  # btn_1이 눌리면 작동할 함수
+  # 시작 버튼
   def button1Function(self):
       global flag
       flag = True
-      #print("switch on")
 
       html = get_html(self.addr.text())
       soup = BeautifulSoup(html, 'html.parser')
 
+      # 받아온 html에서 글 번호만 파싱
       l = soup.find("tbody").find_all("td", class_="gall_num")
 
+      # recent 변수에 현재 최신 글 번호를 저장
       global recent
       recent = 1
-
 
       for idx in l:
           if (not idx.text.isdecimal()):
@@ -106,12 +137,14 @@ class MyApp(QWidget):
           if (recent < int(idx.text)):
               recent = int(idx.text)
 
-
       QMessageBox.about(self, "실행", "알림이 시작 되었습니다.\n이 창을 닫으셔도 좋습니다.")
 
+      # 알리미 수행 도중에도 중지 버튼을 누를 수 있게 쓰레드로 구현
       def run():
           global recent
+          # 중지버튼으로 flag가 false가 되기 전까지 계속 수행
           while flag == True:
+              # 5초 간격으로 get_html() 호출
               time.sleep(5)
               html = get_html(self.addr.text())
               sp = BeautifulSoup(html, 'html.parser')
@@ -121,21 +154,26 @@ class MyApp(QWidget):
               new_name = new_post.find_all("td", class_="gall_writer ub-writer")
               new_num = new_post.find_all("td", class_="gall_num")
 
+              # 새로 가져온 리스트의 글 번호들을 비교
               n_idx = 0
-
               for n in new_num:
                   if (not n.text.isdecimal()):
                       n_idx = n_idx + 1
                       continue
+                  # 새로 가져온 글 번호가 더 크다면, 새로운 글 이라는 뜻
                   if (int(n.text) > recent):
                       recent = int(n.text)
-
                       name = new_name[n_idx].text
                       title = new_title[n_idx].text
-                      toaster.show_toast(title,
-                                         name,
-                                         icon_path="dc_image.ico",
-                                         duration=5)
+                      # 키워드=off 일 경우, 바로 토스트 메시지로 표시
+                      if k_off.isChecked():
+                          toaster.show_toast(title, name, icon_path="dc_image.ico", duration=5)
+                      # 키워드=on 일 경우, 제목에 키워드가 포함 되어있다면 토스트 메시지로 표시
+                      if k_on.isChecked():
+                          for key in range(keyword.count()):
+                              if keyword.item(key).text() in title:
+                                  toaster.show_toast(title, name, icon_path="dc_image.ico", duration=5)
+                                  break
                   n_idx = n_idx + 1
               if flag == False:
                   break
@@ -143,17 +181,21 @@ class MyApp(QWidget):
       thread = threading.Thread(target=run)
       thread.start()
 
-
-
-
-
-      # btn_2가 눌리면 작동할 함수
+  # 중지 버튼
   def button2Function(self):
-      #print("switch off")
       global flag
       flag = False
       QMessageBox.about(self, "중지", "알림이 중지 되었습니다.")
 
+  # 키워드 추가 버튼
+  def button3Function(self):
+      if newItem.text() != '':
+          keyword.addItem(newItem.text())
+
+  # 키워드 삭제 버튼
+  def button4Function(self):
+      select = keyword.currentRow()
+      keyword.takeItem(select)
 
 if __name__ == '__main__':
   app = QApplication(sys.argv)
