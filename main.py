0f61d377a5005473a2140d0823820c9dfddbe8b3
RTFM90172155 <92091737+RTFM90172155@users.noreply.github.com>
2022-09-26 20:00:29 +0900

    [feat] 1.8.1 버전 업데이트

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ import smtplib
 import cerberus
 from email.message import EmailMessage
 
-version = '1.8.0'
+version = '1.8.1'
 
 class get_default_logger(object):
     def __new__(cls):
@@ -135,11 +135,14 @@ def get_p_index():
     pid = os.getpid()
     p_name = psutil.Process(pid).name()
     same_program_pids = list()
+    same_program_ppids = list()
     for proc in psutil.process_iter():
         if proc.name() == p_name:
             same_program_pids.append(proc.pid)
+            same_program_ppids.append(proc.ppid())
+    filtered_pids = [pid for pid in same_program_pids if pid not in same_program_ppids]
     try:
-        return same_program_pids.index(pid)
+        return filtered_pids.index(pid)
     except ValueError:
         return -1
 
@@ -327,7 +330,9 @@ class Notification(QThread):
 
     async def websocket_event_listener(self):
         result = True
-        last_try = 0
+        last_try_time = 0
+        request_interval = 3
+        websocket_interval = 3
         while self.flag:
             try:
                 async with websockets.connect('wss://arca.live/arcalive',
@@ -335,6 +340,7 @@ class Notification(QThread):
                                                 origin='https://arca.live',
                                                 extra_headers=get_scraper().headers) as websocket:
                     try:
+                        websocket_interval = 3
                         self.logger.info('웹소켓 연결 성공')
                         await websocket.send('hello')
                         await websocket.send(f'c|{self.location}')
@@ -342,24 +348,27 @@ class Notification(QThread):
                             try:
                                 message = await asyncio.wait_for(websocket.recv(), timeout=1)
                                 if message == 'na':
+                                    request_interval = 3
                                     self.logger.info('새글 이벤트 감지')
                                     await asyncio.sleep(max(get_p_index(), 0))
-                                    last_try = time.perf_counter()
-                                    if not (result := self.new_article_action()):
-                                        self.notification_action('Unknown', 'Unknown', '')
-                                if not result and time.perf_counter() - last_try > 30:
-                                    self.logger.info('30초 간격으로 실패한 액션 재시도')
-                                    last_try = time.perf_counter()
+                                    last_try_time = time.perf_counter()
+                                    result = self.new_article_action()
+                                if not result and time.perf_counter() - last_try_time > request_interval:
+                                    request_interval = min(request_interval ** 1.5, 60)
+                                    self.logger.info('지수 백오프로 실패한 이벤트 처리 재시도')
+                                    last_try_time = time.perf_counter()
                                     result = self.new_article_action()
                             except asyncio.TimeoutError:
                                 pass
-                    except websockets.ConnectionClosed as e:
+                    except (websockets.ConnectionClosed, RuntimeError) as e:
                         self.logger.warning('웹소켓 연결이 종료되었습니다.', exc_info=e)
-            except (websockets.InvalidURI, websockets.InvalidHandshake) as e:
+            except (websockets.InvalidURI, websockets.InvalidHandshake, asyncio.TimeoutError) as e:
                 self.logger.warning('웹소켓 연결에 실패했습니다.', exc_info=e)
             finally:
                 if self.flag:
-                    await asyncio.sleep(3)
+                    await asyncio.sleep(websocket_interval)
+                    websocket_interval = min(websocket_interval ** 1.5, 60)
+                    self.logger.info('지수 백오프로 웹소켓 연결 재시도')
 
     def new_article_action(self):
         html = get_html(self.url)
